3bc8d8d306b65b97dd28e96591ef1c2226dc01ea
MarkReddy <mark@nextgenreddyholdings.com>
2026-02-15 17:01:58 +0000

    Create page.tsx

---
+++ app/admin/backfill/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminBackfillPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Backfill</h1>
+      <p>If you can see this, /admin/backfill is working.</p>
+    </div>
+  )
+}
