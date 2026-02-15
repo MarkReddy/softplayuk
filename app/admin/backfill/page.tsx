7026ff9d5cd0df3f861cf098972281afe822ef02
MarkReddy <mark@nextgenreddyholdings.com>
2026-02-15 17:16:26 +0000

    Update page.tsx

--- app/admin/backfill/page.tsx
+++ app/admin/backfill/page.tsx
@@ -1,8 +1,5 @@
+import BackfillDashboard from '@/components/admin/backfill-dashboard'
+
 export default function AdminBackfillPage() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Backfill</h1>
-      <p>If you can see this, /admin/backfill is working.</p>
-    </div>
-  )
+  return <BackfillDashboard />
 }
